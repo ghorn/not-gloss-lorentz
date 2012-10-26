c523a57c618afe48cae8ff25b28b3eb9e8fd8dac
Greg Horn <gregmainland@gmail.com>
2012-10-26 10:28:39 +0200

    commented it up

--- src/Main.hs
+++ src/Main.hs
@@ -3,22 +3,18 @@
 module Main where
 
 import SpatialMath
-
 import Vis
 
+-- our state data
 data State a = State (Xyz a) [Xyz a] a
 
-ts :: Double
-ts = 0.01
-
+-- attractor parameters
 rho,sigma,beta :: Double
 rho = 28
 sigma = 10
 beta = 8/3
 
-trailLength :: Int
-trailLength = 2000
-
+-- attractor ODE
 ddt :: Xyz Double -> Xyz Double
 ddt (Xyz x y z) = Xyz x' y' z'
   where
@@ -26,27 +22,42 @@ ddt (Xyz x y z) = Xyz x' y' z'
     y' = x*(rho-z) - y
     z' = x*y- beta*z
 
-integrate :: Xyz Double -> Double -> Xyz Double
-integrate xyz dt = xyz + fmap (*dt) (ddt xyz)
+-- simulation timestep
+ts :: Double
+ts = 0.01
 
+-- length of display trails
+trailLength :: Int
+trailLength = 2000
+
+-- simple forward euler integration
+forwardEulerStep :: Xyz Double -> Xyz Double
+forwardEulerStep xyz = xyz + fmap (*ts) (ddt xyz)
+
+-- simulation function
 simFun :: Float -> State Double -> State Double
-simFun _ (State x trail' simTime0) = State xNext trail (simTime0 + dt)
+simFun _ (State x trail' simTime0) = State xNext trail (simTime0 + ts)
   where
-    dt = ts
-    xNext = integrate x dt
+    xNext = forwardEulerStep x
     trail = take trailLength $ (fmap (*0.1) xNext - (Xyz 0 0 1)):trail'
 
+-- drawing function
 drawFun :: State Double -> VisObject Double
 drawFun (State _ trail simTime) = VisObjects [axes,line,text]
   where
+    -- draw some axes so we know which way's up
     axes = Axes (0.5, 15)
-    line = Line' $ zip trail (map (\a -> makeColor a 0 (1-a) a) (linspace 1 0 (length trail))) -- (makeColor 1 0 0 1)
+    -- draw the trails with varying color and transparency
+    line = Line' $ zip trail (map (\a -> makeColor a 0 (1-a) a) (linspace 1 0 (length trail)))
+    -- draw the simulation time
     text = Text2d ("sim time: "++take 5 (show simTime)) (30,30) TimesRoman24 (makeColor 1 1 1 1)
 
+-- utility function
 linspace :: Fractional a => a -> a -> Int -> [a]
 linspace x0 xf n = map (\k -> x0 + (xf - x0) * (fromIntegral k) / (fromIntegral n-1)) [0..(n-1)]
 
+
 main :: IO ()
-main = simulate Nothing "simulate test" ts state0 drawFun simFun
+main = simulate Nothing "lorentz attractor" ts state0 drawFun simFun
   where
     state0 = State (Xyz 1 1 1) [] 0
