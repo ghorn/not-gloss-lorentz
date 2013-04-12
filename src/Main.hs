e74ee0573ce70f128788b9751715ff11c96655ae
Justin Patera <serialhex@gmail.com>
2013-04-11 22:39:20 -0400

    cool demo of chaos with 2 trails very close to start

--- src/Main.hs
+++ src/Main.hs
@@ -10,9 +10,10 @@ data State a = State (Xyz a) [Xyz a] a
 
 -- attractor parameters
 rho,sigma,beta :: Double
-rho = 28
 sigma = 10
 beta = 8/3
+rho = 28  -- chaotic
+--rho = 14  -- not chaotic
 
 -- attractor ODE
 ddt :: Xyz Double -> Xyz Double
@@ -28,27 +29,38 @@ ts = 0.01
 
 -- length of display trails
 trailLength :: Int
-trailLength = 2000
+trailLength = 500
 
 -- simple forward euler integration
 forwardEulerStep :: Xyz Double -> Xyz Double
 forwardEulerStep xyz = xyz + fmap (*ts) (ddt xyz)
 
+rk4 :: Xyz Double -> Xyz Double
+rk4 xyz = xyz + (1/6) * (k1 + 2*k2 + 2*k3 + k4)
+            where k1 = fmap (*ts) (ddt xyz)
+                  k2 = fmap (*ts) (ddt xyz + 0.5*k1)
+                  k3 = fmap (*ts) (ddt xyz + 0.5*k2)
+                  k4 = fmap (*ts) (ddt xyz + k3)
+
 -- simulation function
-simFun :: Float -> State Double -> State Double
-simFun _ (State x trail' simTime0) = State xNext trail (simTime0 + ts)
+simFun :: State Double -> State Double
+simFun (State x trail' simTime0) = State xNext trail (simTime0 + ts)
   where
-    xNext = forwardEulerStep x
+    xNext = rk4 x
     trail = take trailLength $ (fmap (*0.1) xNext - (Xyz 0 0 1)):trail'
 
+simFun' :: Float -> ((State Double), (State Double)) -> ((State Double), (State Double))
+simFun' _ state = (simFun (fst state), simFun (snd state))
+
 -- drawing function
-drawFun :: State Double -> VisObject Double
-drawFun (State _ trail simTime) = VisObjects [axes,line,text]
+drawFun :: ((State Double), (State Double)) -> VisObject Double
+drawFun ((State _ trail1 simTime), (State _ trail2 _)) = VisObjects [axes,line1,line2,text]
   where
     -- draw some axes so we know which way's up
     axes = Axes (0.5, 15)
     -- draw the trails with varying color and transparency
-    line = Line' $ zip trail (map (\a -> makeColor a 0 (1-a) a) (linspace 1 0 (length trail)))
+    line1 = Line' $ zip trail1 (map (\a -> makeColor a 0 (1-a) a) (linspace 1 0 (length trail1)))
+    line2 = Line' $ zip trail2 (map (\a -> makeColor (1-a) a 0 a) (linspace 1 0 (length trail2)))
     -- draw the simulation time
     text = Text2d ("sim time: "++take 5 (show simTime)) (30,30) TimesRoman24 (makeColor 1 1 1 1)
 
@@ -58,6 +70,6 @@ linspace x0 xf n = map (\k -> x0 + (xf - x0) * (fromIntegral k) / (fromIntegral 
 
 
 main :: IO ()
-main = simulate Nothing "lorentz attractor" ts state0 drawFun simFun
+main = simulate Nothing "lorentz attractor" ts state0 drawFun simFun'
   where
-    state0 = State (Xyz 1 1 1) [] 0
+    state0 = ((State (Xyz 1 1 1) [] 0), (State (Xyz 1.0001 1 1) [] 0))
