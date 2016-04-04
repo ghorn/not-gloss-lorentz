a7e280e494f1d260f18b5164742ea6c3478c9a96
Greg Horn <gregmainland@gmail.com>
2016-04-04 12:14:37 +0200

    revive the code

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,7 @@ import SpatialMath
 import Vis
 
 -- our state data
-data State a = State (Xyz a) [Xyz a] a
+data State a = State (V3 a) [V3 a] a
 
 -- attractor parameters
 rho,sigma,beta :: Double
@@ -16,8 +16,8 @@ rho = 28  -- chaotic
 --rho = 14  -- not chaotic
 
 -- attractor ODE
-ddt :: Xyz Double -> Xyz Double
-ddt (Xyz x y z) = Xyz x' y' z'
+ddt :: V3 Double -> V3 Double
+ddt (V3 x y z) = V3 x' y' z'
   where
     x' = sigma*(y-x)
     y' = x*(rho-z) - y
@@ -32,7 +32,7 @@ trailLength :: Int
 trailLength = 500
 
 -- 4th order Runge-Kutta integration
-rk4 :: Xyz Double -> Xyz Double
+rk4 :: V3 Double -> V3 Double
 rk4 xyz = xyz + (1/6) * (k1 + 2*k2 + 2*k3 + k4)
             where k1 = fmap (*ts) (ddt xyz)
                   k2 = fmap (*ts) (ddt xyz + 0.5*k1)
@@ -44,7 +44,7 @@ simFun :: State Double -> State Double
 simFun (State x trail' simTime0) = State xNext trail (simTime0 + ts)
   where
     xNext = rk4 x
-    trail = take trailLength $ (fmap (*0.1) xNext - (Xyz 0 0 1)):trail'
+    trail = take trailLength $ (fmap (*0.1) xNext - (V3 0 0 1)):trail'
 
 simFun' :: Float -> ((State Double), (State Double)) -> ((State Double), (State Double))
 simFun' _ state = (simFun (fst state), simFun (snd state))
@@ -56,8 +56,8 @@ drawFun ((State _ trail1 simTime), (State _ trail2 _)) = VisObjects [axes,line1,
     -- draw some axes so we know which way's up
     axes = Axes (0.5, 15)
     -- draw the trails with varying color and transparency
-    line1 = Line' $ zip trail1 (map (\a -> makeColor a 0 (1-a) a) (linspace 1 0 (length trail1)))
-    line2 = Line' $ zip trail2 (map (\a -> makeColor (1-a) a 0 a) (linspace 1 0 (length trail2)))
+    line1 = Line' Nothing $ zip trail1 (map (\a -> makeColor a 0 (1-a) a) (linspace 1 0 (length trail1)))
+    line2 = Line' Nothing $ zip trail2 (map (\a -> makeColor (1-a) a 0 a) (linspace 1 0 (length trail2)))
     -- draw the simulation time
     text = Text2d ("sim time: "++take 5 (show simTime)) (30,30) TimesRoman24 (makeColor 1 1 1 1)
 
@@ -67,6 +67,6 @@ linspace x0 xf n = map (\k -> x0 + (xf - x0) * (fromIntegral k) / (fromIntegral 
 
 
 main :: IO ()
-main = simulate Nothing "lorentz attractor" ts state0 drawFun simFun'
+main = simulate defaultOpts ts state0 drawFun simFun'
   where
-    state0 = ((State (Xyz 1 1 1) [] 0), (State (Xyz 1.0001 1 1) [] 0))
+    state0 = ((State (V3 1 1 1) [] 0), (State (V3 1.0001 1 1) [] 0))
